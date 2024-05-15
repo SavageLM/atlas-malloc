6a8d2abcf1ce27b150ed94c18b269a7cd190a607
SavageLM <nagol.egavas@gmail.com>
2024-05-15 14:50:25 -0500

    Updated malloc function and added a navigator function

--- logan.c
+++ logan.c
@@ -1,6 +1,6 @@
 #include "malloc.h"
 
-struct heap_data heap;
+heap_data heap;
 
 /**
  * _malloc - Allocates memory in the heap
@@ -11,19 +11,55 @@ struct heap_data heap;
 void *_malloc(size_t size)
 {
 	static int flag = 0;
-	size_t ptr;
+	size_t aligned_sz = ((size + 7)/8) * 8;
+	blockhead *ptr;
 
 	if (!flag)
 	{
 		heap.first_block =sbrk(0);
 		sbrk(getpagesize());
 		heap.heap_size = getpagesize();
+		heap.first_block->total_bytes = aligned_sz;
+		heap.first_block->used_bytes = aligned_sz;
+		heap.heap_free = heap.heap_size - aligned_sz;
 		heap.numblock = 1;
-		ptr += 17;
+		ptr = heap.first_block += 1;
 		flag = 1;
 		return ((void *) ptr);
 	}
+	if (heap.heap_free - aligned_sz < 0)
+	{
+		sbrk(getpagesize());
+		heap.heap_size += getpagesize();
+		heap.heap_free += getpagesize();
+	}
+	ptr = block_hopper((BLOCK_SZ + aligned_sz));
+	ptr->total_bytes = aligned_sz;
+	ptr->used_bytes = aligned_sz;
+	ptr++;
+	return ((void *)ptr);
+}
 
-	/*Allocate to memory*/
-	
+/**
+ * block_hopper - Function for traversing the header blocks
+ * @size: Size user requests plus blcok header
+ * Return: pointer to big enough chunk for size
+*/
+blockhead *block_hopper(size_t size)
+{
+	size_t i = 0;
+	blockhead *pos;
+
+	pos = heap.first_block;
+	for (; i < heap.numblock; i++)
+	{
+		if (size < (pos->total_bytes - pos->used_bytes))
+		{
+			pos->total_bytes = pos->used_bytes;
+			pos = (blockhead *)((char *)pos + sizeof(blockhead) + pos->total_bytes);
+			return (pos);
+		}
+		pos = (blockhead *)((char *)pos + sizeof(blockhead) + pos->total_bytes);
+	}
+	return (pos);
 }
--- malloc.h
+++ malloc.h
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/*MACROS*/
+#define BLOCK_SZ 16
+
+/*Structs*/
+
 /**
  * struct block_head - stores metadata for each block
  * @total_bytes: total number of bytes occupied
@@ -19,13 +24,15 @@ typedef struct block_head
 /**
  * struct heap_data - Struct for storing heap data
  * @first_block: pointer to first block of metadata
- * @heap_size: Total size of the heap
+ * @heap_size: Total size of the heap in bytes
+ * @heap_free: Amount of heap free to use in bytes
  * @numblock: The number of blocks in the heap
 */
 typedef struct heap_data
 {
 	blockhead *first_block;
 	size_t heap_size;
+	size_t heap_free;
 	size_t numblock;
 } heap_data;
 
@@ -33,5 +40,6 @@ typedef struct heap_data
 void *naive_malloc(size_t size);
 void *_malloc(size_t size);
 void _free(void *ptr);
+blockhead *block_hopper(size_t size);
 
 #endif
