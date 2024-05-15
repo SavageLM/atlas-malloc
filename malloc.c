4843ca6a67e806b0c5d29f74529b91c5e1e33a45
O-01 <sa.an-s@yandex.com>
2024-05-15 16:35:51 -0500

    Modified malloc to accommodate multiple allocations and block splits

--- malloc.c
+++ malloc.c
@@ -12,7 +12,7 @@ void *_malloc(size_t size)
 {
 	static int flag;
 	size_t aligned_sz = ((size + 7) / 8) * 8;
-	blockhead *ptr;
+	blockhead *ptr = NULL;
 
 	if (!flag)
 	{
@@ -36,9 +36,9 @@ void *_malloc(size_t size)
 		sbrk(getpagesize());
 		heap.heap_size += getpagesize(), heap.heap_free += getpagesize();
 	}
+	/* ptr->total_bytes = aligned_sz;
+	ptr->used_bytes = aligned_sz; */
 	ptr = block_hopper((BLOCK_SZ + aligned_sz));
-	ptr->total_bytes = aligned_sz;
-	ptr->used_bytes = aligned_sz;
 	ptr++;
 	heap.numblock++;
 	return ((void *)ptr);
@@ -46,24 +46,32 @@ void *_malloc(size_t size)
 
 /**
  * block_hopper - Function for traversing the header blocks
- * @size: Size user requests plus blcok header
+ * @size: Size user requests plus block header
  * Return: pointer to big enough chunk for size
 */
 blockhead *block_hopper(size_t size)
 {
-	size_t i = 0;
+	size_t i = 0, tmp1 = 0, tmp2 = 0;
 	blockhead *pos;
 
 	pos = heap.first_block;
 	for (; i < heap.numblock; i++)
 	{
+		if (pos->total_bytes > size && !pos->used_bytes)
+			return (pos);
 		if (size < (pos->total_bytes - pos->used_bytes))
 		{
-			pos->total_bytes = pos->used_bytes;
-			pos = (blockhead *)((char *)pos + sizeof(blockhead) + pos->total_bytes);
+			tmp1 = pos->total_bytes;
+			tmp2 = pos->used_bytes;
+			/* pos->total_bytes = pos->used_bytes; */
+			pos = (blockhead *)((char *)pos + sizeof(blockhead) + tmp1);
+			pos->total_bytes = tmp1 - tmp2 - BLOCK_SZ;
+			pos->used_bytes = size - BLOCK_SZ;
 			return (pos);
 		}
 		pos = (blockhead *)((char *)pos + sizeof(blockhead) + pos->total_bytes);
 	}
+	pos->total_bytes = size - BLOCK_SZ;
+	pos->used_bytes = size - BLOCK_SZ;
 	return (pos);
 }
