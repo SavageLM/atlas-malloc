71194afa2ca95c28981ae27f92a4482ef2566a9c
O-01 <sa.an-s@yandex.com>
2024-05-20 15:09:35 -0500

    Modified _realloc to accommodate null input memory, betty formatting guidelines

--- realloc.c
+++ realloc.c
@@ -3,7 +3,6 @@
 /**
  * _realloc - reallocates memory & copies memory to new allocation
  * @ptr: pointer to memory for which space to be reallocated
- * @old_size: size of current allocation
  * @size: expected size of new memory allocation
  * Return: pointer to allocated memory, NULL upon allocation failure
  */
@@ -18,16 +17,19 @@ void *_realloc(void *ptr, size_t size)
 		if (!new_mem)
 			return (NULL);
 	}
-	old_size = (((blockhead *)ptr) - 1)->used_bytes;
-	if (old_size < size)
-	{
-		new_mem = _malloc(size);
-		if (!new_mem)
-			return (NULL);
-		memcpy(new_mem, ptr, old_size);
-		_free(ptr);
-	}
 	else
-		new_mem = ptr;
+	{
+		old_size = (((blockhead *)ptr) - 1)->used_bytes;
+		if (old_size < size)
+		{
+			new_mem = _malloc(size);
+			if (!new_mem)
+				return (NULL);
+			memcpy(new_mem, ptr, old_size);
+			_free(ptr);
+		}
+		else
+			new_mem = ptr;
+	}
 	return (new_mem);
 }
